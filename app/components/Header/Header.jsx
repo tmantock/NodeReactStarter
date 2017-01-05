61c2a74bd4830ff70102032a13b63e5cac9b8aa0
Tevin Mantock <tmantock@gmail.com>
2017-01-04 21:24:13 -0800

    get rid of collapse

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -10,23 +10,16 @@ class Header extends Component {
     return (
         <nav className = "navbar navbar-light" id="navbar">
             <div className = 'navbar-header'>
-                <button className = "navbar-toggle collapsed mdl-badge mdl-badge--overlap" id="nav-toggle" data-toggle="collapse" data-target="#navbar-collapse">
-                    <span className = "icon-bar"></span>
-                    <span className = "icon-bar"></span>
-                    <span className = "icon-bar"></span>
-                </button>
                 <Link to="/" className="navbar-brand">Node React Redux</Link>
             </div>
-            <div className="navbar-collapse collapse navbar-responsive-collapse" id="navbar-collapse">
-                <ul className = "nav navbar-nav">
-                    <li className="nav-item" key={1}>
-                        <Link className="nav-link" to="/about">About</Link>
-                    </li>
-                    <li className="nav-item" key={2}>
-                        <Link className="nav-link" to="/contact">Contact</Link>
-                    </li>
-                </ul>
-            </div>
+            <ul className = "nav navbar-nav">
+                <li className="nav-item" key={1}>
+                    <Link className="nav-link" to="/about">About</Link>
+                </li>
+                <li className="nav-item" key={2}>
+                    <Link className="nav-link" to="/contact">Contact</Link>
+                </li>
+            </ul>
         </nav>
     );
   }
