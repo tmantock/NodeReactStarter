aff9fd75e17718f24ce44211e6187b19890bbea0
Tevin Mantock <tmantock@gmail.com>
2017-02-13 15:37:08 -0800

    ejs templates

--- routes/index.jsx
+++ routes/index.jsx
@@ -9,7 +9,7 @@ import reduxThunk from 'redux-thunk';
 import reducers from '../app/reducers/index.js';
 import routes from './routes.jsx';
 //No longer using ejs, but may in the future
-//router.set('view engine', 'ejs');
+router.set('view engine', 'ejs');
 
 router.get('*', function (request, response) {
 
@@ -27,32 +27,11 @@ router.get('*', function (request, response) {
                     <ReactRouter.RouterContext {...renderProps} />
                 </Provider>
             );
-            response.send(renderFullPage(html, preloadedState));
+            response.render('index.ejs',{ preloadedState });
         } else {
             response.status(404).send('Not Found');
         }
     });
 });
-
-function renderFullPage(html, preloadedState) {
-    //TODO: Use ejs for templating instead of Javascript templating through String Interpolation
- return `
-    <!DOCTYPE html>
-        <html>
-            <head>
-                <title>React Redux Router Starter</title>
-                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css">
-                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap-theme.min.css">
-                <link rel="stylesheet" href="style.css">
-            </head>
-            <body>
-                <div id="root"><div>${html}</div></div>
-                <script type="text/javascript" charset="utf-8">
-                window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState)};
-                </script>
-                <script src="bundle.js"></script>
-            </body>
-        </html>`;
-}
 
 module.exports = router;
